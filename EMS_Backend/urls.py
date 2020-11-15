dc04e8daa192b267d37b53185bcdedd0125f10d1
justpiyoosh <justpiyoosh@gmail.com>
2020-11-16 04:32:23 +0530

    Configured media and static files

--- EMS_Backend/urls.py
+++ EMS_Backend/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path , include
 from posts import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('api/accounts',include('accounts.urls')),
@@ -28,3 +31,7 @@ urlpatterns = [
 
     path('api/account/',include('account.api.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
