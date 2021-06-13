ae70616babd19332cfa05291a75972f34f47ed9c
justpiyoosh <justpiyoosh@gmail.com>
2021-06-13 15:24:58 +0530

    Remove unnessary code

--- EMS_Backend/settings.py
+++ EMS_Backend/settings.py
@@ -109,26 +109,19 @@ AUTH_USER_MODEL = 'account.Account'
 WSGI_APPLICATION = 'EMS_Backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ems',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'ems',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 
 
