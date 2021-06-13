2deb2c215facfc8b7dda516460fa5bfc6fef6966
justpiyoosh <justpiyoosh@gmail.com>
2021-06-13 14:49:28 +0530

    Change settings.py for heroku

--- EMS_Backend/settings.py
+++ EMS_Backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -222,3 +223,14 @@ INTERNAL_IPS = [
     '127.0.0.1',
     # ...
 ]
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
